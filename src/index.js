9d8f666b1f49538c914ba1bd94d0912e715e4d28
Lucas <kjk727@me.com>
2019-01-11 18:36:09 +0900

    add parameter and query

--- src/index.js
+++ src/index.js
@@ -9,8 +9,15 @@ router.get("/", ctx => {
   ctx.body = "home";
 });
 
-router.get("/about", ctx => {
-  ctx.body = "about";
+router.get("/about/:name?", ctx => {
+  const { name } = ctx.params;
+  ctx.body = name ? `${name}의 소개` : "소개";
+});
+
+router.get("/posts", ctx => {
+  const { id } = ctx.query;
+
+  ctx.body = id ? `포스트 #${id}` : "포스트 아이디가 없습니다.";
 });
 
 // app 인스턴스에 라우터 적용
