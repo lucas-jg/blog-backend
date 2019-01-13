dd81447a09b98534435ef852536c0c96aec6572c
Lucas <kjk727@me.com>
2019-01-13 23:34:31 +0900

    Add api functions
    PUT /api/posts/:id

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -68,3 +68,32 @@ exports.remove = ctx => {
   posts.splice(index, 1);
   ctx.status = 204;
 };
+
+/**
+ * 포스트 수정 (교체)
+ * PUT /api/posts/:id
+ * {title, body}
+ */
+
+exports.replace = ctx => {
+  const { id } = ctx.params;
+
+  // 해당 id를 가진 post가 몇 번째인지 확인합니다.
+  const index = posts.findIndex(p => p.id.toString() === id);
+
+  // 포스트가 없으면 오류를 반환합니다.
+  if (index === -1) {
+    ctx.status = 404;
+    ctx.body = {
+      message: "포스트가 존재하지 않습니다."
+    };
+    return;
+  }
+
+  // 전체 객체를 덮어씌웁니다.
+  posts[index] = {
+    id,
+    ...ctx.request.body
+  };
+  ctx.body = posts[index];
+};
