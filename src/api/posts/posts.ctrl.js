9bdab86827c9ef05d09d499ee2d0da7e7a22af47
Lucas <kjk727@me.com>
2019-01-13 23:25:54 +0900

    Add api functions
    GET /api/posts

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -23,3 +23,25 @@ exports.write = ctx => {
   posts.push(post);
   ctx.body = post;
 };
+
+/**
+ * 포스트 목록 조회
+ * GET /api/posts
+ */
+exports.list = cxt => {
+  const { id } = ctx.params;
+
+  // 주어진 id 값으로 포스트를 찾습니다.
+  // 파라미터로 받아 온 값은 문자열 형식이니 파라미터를 숫자로 변환하거나,
+  // 비교할 p.id 값을 문자열로 변경해야 합니다.
+
+  // 포스트가 없으면 오류를 반환합니다.
+  if (!post) {
+    ctx.status = 404;
+    ctx.body = {
+      message: "포스트가 존재하지 않습니다."
+    };
+    return;
+  }
+  ctx.body = post;
+};
