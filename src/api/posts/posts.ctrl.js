8afbf2968d68419ab62c51543673755c53263a9a
Lucas <kjk727@me.com>
2019-01-13 23:47:43 +0900

    Add api functions
    PATCH /api/posts/:id

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -97,3 +97,31 @@ exports.replace = ctx => {
   };
   ctx.body = posts[index];
 };
+
+/**
+ * 포스트 수정 (특정 필드 변경)
+ * PATCH /api/posts/:id
+ * {title, body}
+ */
+exports.update = ctx => {
+  const { id } = ctx.params;
+
+  // 해당 id를 가진 post가 몇 번째인지 확인합니다.
+  const index = posts.findIndex(p => p.id.toString() === id);
+
+  // 포스트가 없으면 오류를 반환합니다.
+  if (index === -1) {
+    ctx.status = 404;
+    ctx.body = {
+      message: "포스트가 존재하지 않습니다."
+    };
+    return;
+  }
+
+  // 기존 값에 정보를 덮어씌웁니다.
+  posts[index] = {
+    ...posts[index],
+    ...ctx.request.body
+  };
+  ctx.body = posts[index];
+};
