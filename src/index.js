611cb617c43ed12d665fd55c3e19ce830cdd9506
Lucas <kjk727@me.com>
2019-01-14 18:22:59 +0900

    apply mongoose

--- src/index.js
+++ src/index.js
@@ -8,6 +8,19 @@ const api = require("./api");
 
 const { PORT: port = 4000, MONGO_URI: mongoURI } = process.env;
 
+const mongoose = require("mongoose");
+
+// Node의 Promise를 사용하도록 설정
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(mongoURI)
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch(e => {
+    console.log("mongoose ERROR : ", e);
+  });
+
 const app = new Koa();
 const router = new Router();
 
