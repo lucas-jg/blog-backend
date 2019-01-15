7a5afc26e056fb58b0ee184647df9af8855162f2
Lucas <kjk727@me.com>
2019-01-15 13:12:19 +0900

    요청 ID 검증 미들웨어 추가

--- src/api/posts/posts.ctrl.js
+++ src/api/posts/posts.ctrl.js
@@ -1,4 +1,17 @@
 const Post = require("models/post");
+const { ObjectId } = require("mongoose").Types;
+
+exports.checkObjectId = (ctx, next) => {
+  const { id } = ctx.params;
+
+  // 검증 실패
+  if (!ObjectId.isValid(id)) {
+    ctx.status = 400;
+    return null;
+  }
+
+  return next();
+};
 /**
  * 포스트 작성
  */
