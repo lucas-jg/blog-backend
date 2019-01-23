bba9fd5236b7f022877b943156c4ab31a576cfc8
Lucas <kjk727@me.com>
2019-01-23 11:10:22 +0900

    Session을 이용한 관리자 로그인 기능 추가

--- src/index.js
+++ src/index.js
@@ -5,21 +5,22 @@ const Router = require("koa-router");
 const bodyParser = require("koa-bodyparser");
 
 const api = require("./api");
+const session = require("koa-session");
 
-const { PORT: port = 4000, MONGO_URI: mongoURI } = process.env;
+const { PORT: port = 4000, MONGO_URI: mongoURI, COOKIE_SIGN_KEY: signKey } = process.env;
 
 const mongoose = require("mongoose");
 
 // Node의 Promise를 사용하도록 설정
 mongoose.Promise = global.Promise;
 mongoose
-  .connect(mongoURI)
-  .then(() => {
-    console.log("connected to mongodb");
-  })
-  .catch(e => {
-    console.log("mongoose ERROR : ", e);
-  });
+	.connect(mongoURI)
+	.then(() => {
+		console.log("connected to mongodb");
+	})
+	.catch(e => {
+		console.log("mongoose ERROR : ", e);
+	});
 
 const app = new Koa();
 const router = new Router();
@@ -31,9 +32,18 @@ router.use("/api", api.routes());
 // 미들웨어의 경우 라우터 적용전에 설정해야함
 app.use(bodyParser());
 
+// 세션/키 적용
+const sessionConfig = {
+	maxAge: 86400000 // 하루
+	// signed: true  // default
+};
+
+app.use(session(sessionConfig, app));
+app.keys = [signKey];
+
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(port, () => {
-  console.log("listening to port ", port);
+	console.log("listening to port ", port);
 });
