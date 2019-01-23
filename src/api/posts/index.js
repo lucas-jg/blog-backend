e11c8068ccebbc231f7e63a720fd89cd7dabf236
Lucas <kjk727@me.com>
2019-01-23 11:14:13 +0900

    API 호출시 로그인 인증 확인 단계 추가

--- src/api/posts/index.js
+++ src/api/posts/index.js
@@ -3,17 +3,18 @@ const postsCtrl = require("./posts.ctrl");
 const posts = new Router();
 
 const Info = ctx => {
-  ctx.body = {
-    method: ctx.method,
-    path: ctx.path,
-    params: ctx.params
-  };
+	ctx.body = {
+		method: ctx.method,
+		path: ctx.path,
+		params: ctx.params
+	};
 };
 
 posts.get("/", postsCtrl.list);
-posts.post("/", postsCtrl.write);
 posts.get("/:id", postsCtrl.checkObjectId, postsCtrl.read);
-posts.delete("/:id", postsCtrl.checkObjectId, postsCtrl.remove);
-posts.patch("/:id", postsCtrl.checkObjectId, postsCtrl.update);
+
+posts.post("/", postsCtrl.checkLogin, postsCtrl.write);
+posts.delete("/:id", postsCtrl.checkLogin, postsCtrl.checkObjectId, postsCtrl.remove);
+posts.patch("/:id", postsCtrl.checkLogin, postsCtrl.checkObjectId, postsCtrl.update);
 
 module.exports = posts;
