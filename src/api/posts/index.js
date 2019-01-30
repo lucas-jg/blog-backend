d0b7c66fc42c62156a3f15e3a3813a411d0d5a8e
Lucas <kjk727@me.com>
2019-01-30 10:41:27 +0900

    불필요 코드 삭제

--- src/api/posts/index.js
+++ src/api/posts/index.js
@@ -2,17 +2,8 @@ const Router = require("koa-router");
 const postsCtrl = require("./posts.ctrl");
 const posts = new Router();
 
-const Info = ctx => {
-	ctx.body = {
-		method: ctx.method,
-		path: ctx.path,
-		params: ctx.params
-	};
-};
-
 posts.get("/", postsCtrl.list);
 posts.get("/:id", postsCtrl.checkObjectId, postsCtrl.read);
-
 posts.post("/", postsCtrl.checkLogin, postsCtrl.write);
 posts.delete("/:id", postsCtrl.checkLogin, postsCtrl.checkObjectId, postsCtrl.remove);
 posts.patch("/:id", postsCtrl.checkLogin, postsCtrl.checkObjectId, postsCtrl.update);
